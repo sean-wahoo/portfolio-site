6d367c79f362100897197f192d75f2429062f123
= <=>
2021-09-24 07:53:44 -0400

    created selection context

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,14 @@
-import React from "react";
 import styles from "styles/App.module.scss";
 import ReactMarkdown from "react-markdown";
+import TabList from "components/TabList";
+import { useContext } from "react";
+import SelectionContext from "utils/SelectionContext";
 
-export default function Content(props: any) {
-    return <div className={styles.content}>{props.page}</div>;
+export default function Content() {
+    const selection = useContext(SelectionContext);
+    return (
+        <div className={styles.content}>
+            <TabList tabSelected={selection} />
+        </div>
+    );
 }
