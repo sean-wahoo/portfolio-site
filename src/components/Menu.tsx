2d1f0a09f56ec173d74d03926420449be4209bb3
= <=>
2021-09-25 14:38:03 -0400

    added menu version of socials

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -58,6 +58,29 @@ export default function Menu() {
                         </li>
                     </>
                 )}
+                <div className={styles.socials}>
+                    <a
+                        href="https://github.com/sean-wahoo"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className={`${styles.icon} fab fa-github`}></i>
+                    </a>
+                    <a
+                        href="mailto:sean.p.reichel@gmail.com?subject=FreelanceProject"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className={`${styles.icon} fas fa-envelope`}></i>
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/sean-reichel-a6a9461a7/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className={`${styles.icon} fab fa-linkedin`}></i>
+                    </a>
+                </div>
             </div>
         </SelectionContext.Provider>
     );
