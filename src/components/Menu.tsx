5c1865e5856dd0bdac4e1eb20e1c56ef1d924962
= <=>
2021-09-26 04:24:51 -0400

    removed console log

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,13 @@
 import { useState, useContext } from "react";
 import SelectionContext from "utils/SelectionContext";
 import styles from "styles/App.module.scss";
+import Footer from "components/Footer";
 
 export default function Menu() {
     const [folder, toggleFolder] = useState(true);
     const { selection, setSelection } = useContext(SelectionContext);
 
     const handleSelectionChange = (selection: string) => {
-        console.log(selection);
         setSelection(selection);
     };
 
@@ -58,29 +58,7 @@ export default function Menu() {
                         </li>
                     </>
                 )}
-                <div className={styles.socials}>
-                    <a
-                        href="https://github.com/sean-wahoo"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <i className={`${styles.icon} fab fa-github`}></i>
-                    </a>
-                    <a
-                        href="mailto:sean.p.reichel@gmail.com?subject=FreelanceProject"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <i className={`${styles.icon} fas fa-envelope`}></i>
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/sean-reichel-a6a9461a7/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <i className={`${styles.icon} fab fa-linkedin`}></i>
-                    </a>
-                </div>
+                <Footer isMenu={true} />
             </div>
         </SelectionContext.Provider>
     );
